01f628b061bd8e583fb523a01186a158012fb8ba
kankan-web <2728360489@qq.com>
2025-02-21 16:31:35 +0800

    feat：Typescript增强

--- src/typings/components.d.ts
+++ src/typings/components.d.ts
@@ -34,6 +34,8 @@ declare module 'vue' {
     ElSpace: typeof import('element-plus/es')['ElSpace']
     ElSpce: typeof import('element-plus/es')['ElSpce']
     ElSubMenu: typeof import('element-plus/es')['ElSubMenu']
+    ElTable: typeof import('element-plus/es')['ElTable']
+    ElTableColumn: typeof import('element-plus/es')['ElTableColumn']
     ElTableV2: typeof import('element-plus/es')['ElTableV2']
     ElTabPane: typeof import('element-plus/es')['ElTabPane']
     ElTabs: typeof import('element-plus/es')['ElTabs']
